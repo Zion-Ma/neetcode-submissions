0373a76275520d32edd6447ed993faa9f0027338
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-06-22 18:21:43 -0400

    Add: design-twitter-feed - submission-7

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-7.py
@@ -0,0 +1,37 @@
+class Twitter:
+
+    def __init__(self):
+       self.timestamp = 0
+       self.tweet_heap = []
+       self.followee = defaultdict(set)
+       self.tweet = defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        heapq.heappush(self.tweet_heap, (self.timestamp, userId, tweetId))
+        self.tweet[userId].append(tweetId)
+        self.timestamp -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        count = 0
+        popped = []
+        feed = []
+        print(f"retriving for {userId}")
+        while self.tweet_heap and count < 10:
+            timestamp, poster_id, tweetId = heapq.heappop(self.tweet_heap)
+            print(f"currently: {poster_id}:{tweetId}")
+            if poster_id in self.followee[userId] or poster_id == userId:
+                feed.append(tweetId)
+                print("qualified")
+                count += 1
+            popped.append((timestamp, poster_id, tweetId))
+        for item in popped:
+            heapq.heappush(self.tweet_heap, item)
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followee[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followee[followerId]:
+            self.followee[followerId].remove(followeeId)
+        
