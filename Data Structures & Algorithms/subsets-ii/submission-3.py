f33918e65787a10ebae975821e2da0ab9657e936
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-06-23 22:43:32 -0400

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        def select(curr_list: list, curr_idx: int) -> None:
+            if curr_idx == len(nums):
+                ans.append(curr_list.copy())
+                return
+            curr_list.append(nums[curr_idx])
+            select(curr_list, curr_idx + 1)
+            curr_list.pop()
+            while curr_idx + 1 < len(nums) and nums[curr_idx] == nums[curr_idx + 1]:
+                curr_idx += 1
+            select(curr_list, curr_idx + 1)
+            return
+        select([], 0)
+        return ans
+                
+            
