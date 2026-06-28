01fead99f1bc0454333dcc44f51578ae14f5758a
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-06-28 19:57:28 -0400

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            # Max = -1001
+            for j in range(i):
+                # using Max to avoid number that has been represented by lager numbers
+                if nums[j] >= nums[i]:
+                    continue
+                dp[i] = max(dp[i], 1 + dp[j])
+                # Max = nums[j]
+        return max(dp)
+
+            
