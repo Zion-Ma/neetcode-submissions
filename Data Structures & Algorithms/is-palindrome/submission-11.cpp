37bdb93a8d55a68a83354c973a3b29925aad9f07
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-08-01 02:06:41 -0400

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = (int)s.size() - 1;
+        while (left < right) {
+            while (left < right and !alphaNum(s[left])) {
+                left++;
+            }
+            while (left < right and !alphaNum(s[right])) {
+                right--;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool alphaNum(char c) {
+        return std::isalpha(c) or std::isdigit(c);
+    }
+};
