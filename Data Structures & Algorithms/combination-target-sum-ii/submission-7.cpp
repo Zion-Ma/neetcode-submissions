55d397758ed06ab546d43b7cd7631e7471226a4f
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-08-02 23:06:59 -0400

    Add: combination-target-sum-ii - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-7.cpp
@@ -0,0 +1,32 @@
+// #include <numeric> 
+
+class Solution {
+public:
+    int len;
+    int t;
+    vector<vector<int>> result;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        len = (int)candidates.size();
+        t = target;
+        vector<int> curr;
+        sort(candidates.begin(), candidates.end());
+        dfs(0, curr, candidates);
+        return result;
+    }
+    void dfs(int idx, vector<int>& curr, vector<int>& candidates) {
+        if (std::accumulate(curr.begin(), curr.end(), 0) == t) {
+            result.emplace_back(curr);
+            return;
+        }
+        if (idx == len or std::accumulate(curr.begin(), curr.end(), 0) > t) {
+            return;
+        }
+        curr.push_back(candidates[idx]);
+        dfs(idx + 1, curr, candidates);
+        curr.pop_back();
+        while (idx + 1 < len and candidates[idx] == candidates[idx + 1]) {
+            idx++;
+        }
+        dfs(idx + 1, curr, candidates);
+    }
+};
