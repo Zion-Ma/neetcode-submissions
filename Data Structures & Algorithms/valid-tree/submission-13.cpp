e04f54408a759b863fec1c98318dfbbccc4ad8c6
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-08-04 18:00:38 -0400

    Add: valid-tree - submission-13

---
+++ Data Structures & Algorithms/valid-tree/submission-13.cpp
@@ -0,0 +1,48 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> size;
+    int component;
+    DSU(int p) {
+        for (int i = 0; i < p; i++) {
+            parent.push_back(i);
+        }
+        size.assign(p, 1);
+        component = p;
+    }
+    int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+    bool union_find(int i, int j) {
+        int p = find(i), q = find(j);
+        if (p == q) {
+            return false;
+        }
+        if (size[p] > size[q]) {
+            parent[q] = p;
+            size[p] += size[q];
+        } else {
+            parent[p] = q;
+            size[q] += size[p];
+        }
+        component -= 1;
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if ((int)edges.size() != n - 1) {return false;};
+        DSU dsu(n);
+        for (const auto& e : edges) {
+            if (!dsu.union_find(e[0], e[1])) {
+                return false;
+            }
+        }
+        return dsu.component == 1;
+    }
+};
