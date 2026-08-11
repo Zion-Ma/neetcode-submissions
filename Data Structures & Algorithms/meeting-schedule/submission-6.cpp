f2687e3ac3a53259f2034d72fd53e8d00a7add31
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-08-11 00:14:14 -0400

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(
+            intervals.begin(), intervals.end(), [](const Interval& x, const Interval& y){
+                return x.start < y.start;
+            }
+        );
+        int prev_end = intervals[0].end;
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].start < prev_end) {
+                return false;
+            }
+            prev_end = intervals[i].end;
+        }
+        return true;
+    }
+};
