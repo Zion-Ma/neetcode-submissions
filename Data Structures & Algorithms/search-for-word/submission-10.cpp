790b61f473ef2a22330cf9f4a7cac09310742fbe
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2026-08-13 14:40:36 -0400

    Add: search-for-word - submission-10

---
+++ Data Structures & Algorithms/search-for-word/submission-10.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int target_length, m, n;
+    bool exist(vector<vector<char>>& board, string word) {
+        target_length = (int)word.size();
+        m = (int)board.size();
+        n = (int)board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j, 0, board, word)) {return true;}
+            }
+        }
+        return false;
+    }
+    bool dfs(int i, int j, int k, vector<vector<char>>& board, const string& word) {
+        if (k >= target_length) {return true;}
+        if (
+            i >= m or i < 0 or\
+            j >= n or j < 0 or\
+            board[i][j] != word[k]
+        ) {return false;}
+        char temp_char = board[i][j];
+        board[i][j] = '.';
+        if (
+            dfs(i + 1, j, k + 1, board, word) or\
+            dfs(i - 1, j, k + 1, board, word) or\
+            dfs(i, j + 1, k + 1, board, word) or\
+            dfs(i, j - 1, k + 1, board, word) 
+        ) {return true;}
+        board[i][j] = temp_char;
+        return false;
+    }
+};
